697225fdac9f938b2ddd5164326109cbd93ea9c6
BrunoRaya <brunoraya00@gmail.com>
2024-11-29 18:37:07 -0300

    changes 3

--- routes/professionals.js
+++ routes/professionals.js
@@ -5,18 +5,18 @@ const router = express.Router();
 
 router.get('/professionals', async (req, res) => {
   try {
-    const professions = req.query.professions?.split(',') || [];
+    const professions = req.query.professions?.split(',') || []; 
 
     if (professions.length === 0) {
       return res.status(400).json({ message: 'Nenhuma profissão fornecida.' });
     }
 
-    const regexQueries = professions.map(prof => new RegExp(`(^|,\\s*)${prof}(\\s*,|$)`, 'i'));
-
     const professionals = await Professional.find({
-      profession: { $in: regexQueries }
+      $or: professions.map(prof => ({
+        profession: { $regex: `(^|,\\s*)${prof}(\\s*,|$)`, $options: 'i' }
+      }))
     })
-      .select('name email profession -_id'); 
+      .select('name email profession -_id');
 
     if (professionals.length === 0) {
       return res.status(404).json({ message: 'Nenhum profissional encontrado para as profissões fornecidas.' });
