3c325296a997599dbc836f7f45c292fd8a6f95fd
BrunoRaya <brunoraya00@gmail.com>
2024-11-29 18:02:23 -0300

    testes 2

--- routes/professionals.js
+++ routes/professionals.js
@@ -3,17 +3,22 @@ import Professional from '../models/professional.js';
 
 const router = express.Router();
 
-// Rota para buscar profissionais com base nas profissões
 router.get('/professionals', async (req, res) => {
-  try {
-    const professions = req.query.professions?.split(',') || [];
-    const professionals = await Professional.find({ profession: { $in: professions } })
-      .select('name email profession');
-    res.json(professionals);
-  } catch (error) {
-    console.error('Erro ao buscar profissionais:', error);
-    res.status(500).json({ message: 'Erro no servidor' });
-  }
-});
+    try {
+      const professions = req.query.professions?.split(',') || [];
+  
+      // Realizando a busca insensível a maiúsculas/minúsculas
+      const professionals = await Professional.find({
+        profession: { $in: professions.map(prof => new RegExp(`^${prof}$`, 'i')) }
+      })
+        .select('name email profession');
+  
+      res.json(professionals);
+    } catch (error) {
+      console.error('Erro ao buscar profissionais:', error);
+      res.status(500).json({ message: 'Erro no servidor' });
+    }
+  });
+  
 
 export default router;
--- server.js
+++ server.js
@@ -4,13 +4,11 @@ import dotenv from 'dotenv';
 import professionalRoutes from './routes/professionals.js';
 import Professional from './models/professional.js';
 
-// Carregando variáveis de ambiente
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4200;
 
-// Conectando ao MongoDB
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,15 +16,13 @@ mongoose.connect(process.env.DATABASE_URL, {
   .then(() => console.log('Conectado ao MongoDB!'))
   .catch(err => console.log('Erro de conexão com MongoDB:', err));
 
-// Middleware para ler o corpo das requisições como JSON
 app.use(express.json());
 
-// Rota para buscar profissionais com base nas profissões
 app.get('/api/professionals', async (req, res) => {
   try {
-    const professions = req.query.professions?.split(',') || []; // Recebe as profissões como query
+    const professions = req.query.professions?.split(',') || []; 
     const professionals = await Professional.find({ profession: { $in: professions } })
-      .select('name email profession'); // Seleciona apenas os campos necessários
+      .select('name email profession'); 
     res.json(professionals);
   } catch (error) {
     console.error('Erro ao buscar profissionais:', error);
@@ -34,10 +30,8 @@ app.get('/api/professionals', async (req, res) => {
   }
 });
 
-// Usando as rotas de profissionais
 app.use('/api', professionalRoutes);
 
-// Iniciando o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
